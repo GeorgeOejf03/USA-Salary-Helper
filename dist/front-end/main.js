a05022a1334433f608f9caaecca54e8dadeb8e9d
Youssef <youssef1993fayez@gmail.com>
2019-06-28 22:14:19 -0500

    updated link

--- dist/front-end/main.js
+++ dist/front-end/main.js
@@ -390,7 +390,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    baseUrl: 'https://peaceful-caverns-15103.herokuapp'
+    baseUrl: 'https://peaceful-caverns-15103.herokuapp.com'
 };
 /*
  * For easier debugging in development mode, you can import the following file
