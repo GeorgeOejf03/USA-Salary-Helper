7c57b1f36355696d6b15cb81f26b6d6c4875fd84
Youssef <youssef1993fayez@gmail.com>
2019-06-30 16:32:36 -0500

    dasd

--- dist/front-end/main.js
+++ dist/front-end/main.js
@@ -410,7 +410,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    baseUrl: 'https://peaceful-caverns-15103.herokuapp.com'
+    baseUrl: 'https://evening-bayou-86060.herokuapp.com'
 };
 /*
  * For easier debugging in development mode, you can import the following file
